27249409f9556f14ad0dfaf9a6ad21eeec10cd8e
lindsey-s <ly.stevenson@gmail.com>
2020-01-21 14:10:58 -0800

    upgrade to babel 7x to fix failing tests and build

--- src/components/USAState.js
+++ src/components/USAState.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 const USAState = (props) => {
+  const className = `${props.state} state`
   return (
-    <path d={props.dimensions} fill={props.fill} data-name={props.state} className={`${props.state} state`} onClick={props.onClickState}>
+    <path d={props.dimensions}
+      fill={props.fill}
+      data-name={props.state}
+      className={className}
+      onClick={props.onClickState}>
       <title>{props.stateName}</title>
     </path>
   );
